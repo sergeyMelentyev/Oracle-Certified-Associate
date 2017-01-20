625c29aff45616d53d5215018fadb663b6ebea02
Sergey Melentyev <sergey.melentyev@gmail.com>
2017-01-20 13:48:27 +0300

    Update main.java

--- main.java
+++ main.java
@@ -21,6 +21,8 @@ class newClassName {
 	innerClassName b; }	// will not compile
 import packageName.*;	// import all public members, classes and interfaces. Previous will compile
 
+
+
 /* ACCESS MODIFIER */
 // top-level class and interface camnnot have defined with protected and private access
 // method parameters and local vars cannot be defined using explicit access modifiers
@@ -30,10 +32,14 @@ public;	// can be accessed from everywere, different package and not derived cla
 protected;	// cannot be accessed from not derived class in different package
 default;	// access only from the same package
 
+
+
 /* ABSTRACT MODIFIER */
 // none of the different types of variable can be defined as abstract, will not compile
 abstract class ClassName {}	// cannot be instantiated, may not containe any abstract methods
 interface InterfaceName {}	// abstract keyword is allowed but redundant
+
+
 
 /* FINAL MODIFIER */
 // class, method or variable can be final. Cannot be used with the declaration of an interface
@@ -41,12 +47,16 @@ final class ClassName {}	// cannot be extended
 final int i;	// val assigne once via declaration or in constructor calling methods that change its state
 final methodName() {}	// cannot be overridden by a derived class
 
+
+
 /* STATIC MODIFIER */
 // class, method (not top level), variable or interface (not top level) can be static. 
 // nonprivate static vars and methods can be inherited, cannot be overrided but can be redefined
 static int i;	// shared with all instances, can be accessed when no instances have been created
 static void methodName(){}	// cannot access instance vars, only static.
 ClassName name = null; name.staticVarName;	// can access static var and methods using null reference
+
+
 
 /* PRIMITIVE DATA TYPE */
 Numetic; Signed; Integers; byte, short, int, long;	// byte -128 to 127 inclusive
@@ -56,10 +66,42 @@ Boolean; boolean;
 int octal = 0413; int hex = 0x10B; int binary = 0b10010;
 int octal = 04_13; int hex = 0x10_BA_75; int binary = 0b1_0000_10;	// valid literal values
 
+byte b = 10; short s = b + b;	// will not compile, possible lossy conversion, b will be widened to int
+final byte b = 10; short s = b + b;	// will compile.
+
+// postfix and prefix operators behave the same if not part of an expression
+int a = 10; a = a++ + a + a-- - a-- + ++a;	// a = 10 + 11 + 11 - 10 + 10;
+
+/* Precedence of operators
+	exp++, exp--
+	++exp, --exp, +exp, -exp
+	*, /, %
+	+, -
+	<, >, <=, >=
+	==, !=
+	&&
+	||
+	=, +=, -=, *=, /=, %=
+*/
 
 
 
+/* OBJECT REFERENCE VARIABLE */
+// store the address of the obj they refer to
+ClassName className = new ClassName();	// Type of obj reference var, name of obj reference var
 
+
+
+/* WRAPPER CLASSES. IMMUTABLE OBJ */
+// Object -> Boolean, Character
+// Object -> Number -> Byte, Chort, Integer, Long, Float, Double
+Boolean bool = true; Byte b = 10;	// autoboxing
+Boolean bool = new Boolean(true); Byte b = new Byte((byte)10);	// constructor with primitive values
+Boolean bool = new Boolean("true"); Byte b = new Byte("10");	// constructor with string values
+Boolean bool = Boolean.valueOf(true); Byte b = Byte.valueOf(10);	// static mehod
+booleanValue(); charValue(); byteValue();	// retreive primitive value
+parseBoolean(String s); parseByte(String s);	// get primitive val from string, EXEPT for Character
+// all parse methods throws NumberFormat-Exception except Boolean.parseBoolean(), that returns false
 
 
 class ClassName {
@@ -69,12 +111,7 @@ class ClassName {
 	}
 }
 
-
 interface InterfaceName {
 	static methodName(){};
 	default methodName(){};
 }
-
-
-
-
